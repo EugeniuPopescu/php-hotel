6ac4010aaa7d97bc520171b127dfbb7dc4572150
Eugen <epopescuu@yahoo.it>
2023-12-18 17:40:35 +0100

    first commit

---
+++ alberghi.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hotels</title>
+
+        <!-- link bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+
+    <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+
+        ];
+
+    ?>
+
+    <body class="text-white bg-dark">
+        <div class="container">
+            <h1 class="text-center my-3">Hotels</h1>
+
+            <?php
+                foreach ($hotels as $hotel) {
+                    echo $name = $hotel['name'];
+                    echo '<br>';
+                    echo $description = $hotel['description'];
+                    echo '<br>';
+
+                    // ciclo if parcheggio 
+                    if ($hotel['parking'] === true) {
+                        echo $parking = 'Yes private parking';
+                    } else {
+                        echo $parking = 'No private parking';
+                    }
+                
+                    echo '<br>';
+                    echo $vote = $hotel['vote'];
+                    echo '<br>';
+                    echo $distance_to_center = $hotel['distance_to_center'];
+                    echo '<br>';
+                    echo '-------------';
+                    echo '<br>';
+                }
+            ?>
+        </div>
+    </body>
+
+    <!-- script bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
